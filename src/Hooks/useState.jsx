e152146a16f850593fbd86ba95e5c5cd89f3723c
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-04 14:11:18 +0200

    feat: added functionality to show more details

--- src/Hooks/useState.jsx
+++ src/Hooks/useState.jsx
@@ -4,17 +4,31 @@ import { sculptureList } from './data'
 
 export const Gallery = () => {
     const [index, setIndex] = useState(0)
+    const [showMore, setShowMore] = useState(false)
 
     let sculpture = sculptureList[index]
+
+    const handleIndex = () => {
+        setIndex(index + 1)
+    }
+
     return (
         <>
-            <input type="button" value="next" />
+            <input type="button" value="next" onClick={() => handleIndex()} />
             <div className="">
+                <h1>{sculpture.name} by {sculpture.artist}</h1>
+                <p>{index + 1} of {sculptureList.length}</p>
+                <div>
+                    <input type="button" value="Show More" onClick={() => setShowMore(prevState => !prevState)} />
+                </div>
                 <img src={sculpture.url} alt={sculpture.alt} />
                 <hr />
-                <h1>{sculpture.name}</h1>
-                <p>{sculpture.artist}</p>
-                <p>{sculpture.description}</p>
+                {
+                    showMore
+                        ? <p>{sculpture.description}</p>
+
+                        : <div></div>
+                }
             </div>
         </>
     )
