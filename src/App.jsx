f1b5852cc0c3679e071fbc07a98051d68b5152bb
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-05 16:37:35 +0200

    feat: added initialProduct

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-import Todo from "./Hooks/useState/todo/Todo"
+import ShoppingCart from './Hooks/useState/shoppingCart/ShoppingCart'
 
 export default function App() {
+  const initialProducts = [{
+    id: 0,
+    name: 'Baklava',
+    count: 1,
+  }, {
+    id: 1,
+    name: 'Cheese',
+    count: 5,
+  }, {
+    id: 2,
+    name: 'Spaghetti',
+    count: 2,
+  }];
   return (
-    <Todo />
+    <ShoppingCart />
   )
 }
